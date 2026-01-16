fedd9ef2f01bace642592bd6a5df8c748e5d8f4e
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-01-16 11:17:36 +0000

    A Remap of stuffs

--- init.lua
+++ init.lua
@@ -36,7 +36,11 @@ require 'himadri.himadri'
 vim.keymap.set('n', 'z', ':Zcd ', { noremap = true })
 vim.keymap.set('n', '<leader>.', ':Zt<CR>', { noremap = true, silent = true })
 vim.keymap.set('n', '<leader>e', ':Oil<CR>', { noremap = true, silent = true })
-vim.keymap.set('n', '<leader>gg', ':Git<CR>', { noremap = true, silent = true })
+
+vim.keymap.set('n', '<leader>g', ':Git<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gp', ':!git push<CR>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>gP', ':!git pull<CR>', { noremap = true, silent = true })
+
 vim.keymap.set('n', ';;', ':w<CR>', { noremap = true, silent = true })
 
 require 'theradlectures.terminalflaot'
@@ -139,6 +143,7 @@ require('lazy').setup({
           buffers = {
             ignore_current_buffer = true,
             sort_lastused = true,
+            -- layout_strategy = 'vertical',
             layout_config = {
               preview_width = 0.7,
             },
@@ -155,11 +160,11 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>o', builtin.find_files, { desc = '[S]earch [F]iles' })
-      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
-      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>fs', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>fw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>f', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
-      vim.keymap.set('n', '<leader>go', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader>oo', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
