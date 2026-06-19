9020c7d9a7d2d3406918c09d0752c4bd63ca8941
HimadriChakra12 <himadrichakrabortydip@gmail.com>
2026-06-19 12:54:22 +0600

    added theme

--- init.lua
+++ init.lua
@@ -25,6 +25,24 @@ o.wrap = true
 o.linebreak = true
 o.path = '**'
 
+-- Transparent background
+vim.api.nvim_set_hl(0, "Normal", { fg = "#ebdbb2", bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "LineNr", { fg = "#7c6f64", bg = "none" })
+vim.api.nvim_set_hl(0, "EndOfBuffer", { fg = "#3c3836", bg = "none" })
+
+-- Gruvbox-like syntax colors
+vim.api.nvim_set_hl(0, "Comment", { fg = "#928374", italic = true })
+vim.api.nvim_set_hl(0, "String", { fg = "#b8bb26" })
+vim.api.nvim_set_hl(0, "Function", { fg = "#fabd2f" })
+vim.api.nvim_set_hl(0, "Keyword", { fg = "#fb4934" })
+vim.api.nvim_set_hl(0, "Type", { fg = "#8ec07c" })
+vim.api.nvim_set_hl(0, "Constant", { fg = "#d3869b" })
+vim.api.nvim_set_hl(0, "Identifier", { fg = "#83a598" })
+vim.api.nvim_set_hl(0, "PreProc", { fg = "#fe8019" })
+
+
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
